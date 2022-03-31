7410c416455e48327482dc4dbd469804bfa440d9
Lao-Liu233 <laoliu735@gmail.com>
2022-03-31 23:28:13 +0800

    Add&Fix: DB things

--- sql/init_sqlite.py
+++ sql/init_sqlite.py
@@ -1,8 +1,8 @@
 import sqlite3
 
-conn = sqlite3.connect('data.db')
+conn = sqlite3.connect('./accounts.db')
 c = conn.cursor()
-c.execute("CREATE TABLE IF NOT EXISTS twitter_users (id INTEGER PRIMARY KEY, name TEXT, url TEXT)")
-c.execute("CREATE TABLE IF NOT EXISTS pixiv_users (id INTEGER PRIMARY KEY, name TEXT, url TEXT)")
+c.execute("CREATE TABLE IF NOT EXISTS twitter_users (id INTEGER PRIMARY KEY, name TEXT)")
+c.execute("CREATE TABLE IF NOT EXISTS pixiv_users (id INTEGER PRIMARY KEY, name TEXT)")
 conn.commit
 conn.close()
--- sql/sqlite.py
+++ sql/sqlite.py
@@ -1,30 +1,45 @@
-from cgi import test
-import re
+from distutils.util import execute
 import sqlite3
 
 class database: 
     def __init__(self):
-        self.conn = sqlite3.connect('data.db')
+        pass
+        
+    def get_all_twitter_user_id(self):
+        self.conn = sqlite3.connect('accounts.db')
         self.c = self.conn.cursor()
+        self.c.execute("SELECT id FROM twitter_users")
+        return_data = self.c.fetchall()
+        self.conn.commit()
+        self.conn.close()
+        return return_data  # [(id,), (id,), ...]
     
-    # type: 0-twitter 1-pixiv
-    def add_user(self, type, id, name):
-        if type == 0:
-            self.c.execute("SELECT id FROM twitter_users WHERE id=?", (id,))
-            if len(list(self.c)) == 0:
-                self.c.execute("INSERT INTO twitter_users (id, name) VALUES (?, ?)", (id, name))
-            else:
-                return False
-        elif type == 1:
-            self.c.execute("SELECT id FROM pixiv_users WHERE id=?", (id,))
-            if len(list(self.c)) == 0:
-                self.c.execute("INSERT INTO pixiv_users (id, name) VALUES (?, ?)", (id, name))
-            else:
-                return False
+    def add_twitter_user(self,twitter_id, name):
+        self.conn = sqlite3.connect('accounts.db')
+        self.c = self.conn.cursor()
+        self.c.execute("SELECT id FROM twitter_users WHERE id=?", (twitter_id,))
+        if len(list(self.c)) == 0:
+            self.c.execute("INSERT INTO twitter_users (id, name) VALUES (?, ?)", (twitter_id, name))
+            self.conn.commit()
+            self.conn.close()
+            self.add_new_twitter_user_database(name)
+        else:
+            return False
+        
+    def add_pixiv_user(self, pixiv_id, name):
+        self.conn = sqlite3.connect('accounts.db')
+        self.c = self.conn.cursor()
+        self.c.execute("SELECT id FROM pixiv_users WHERE id=?", (pixiv_id,))
+        if len(list(self.c)) == 0:
+            self.c.execute("INSERT INTO pixiv_users (id, name) VALUES (?, ?)", (pixiv_id, name))
+        else:
+            return False
         self.conn.commit()
         self.conn.close()
         
     def del_user(self, type, id):
+        self.conn = sqlite3.connect('accounts.db')
+        self.c = self.conn.cursor()
         if type == 0:
             self.c.execute("SELECT id FROM twitter_users WHERE id=?", (id,))
             if len(list(self.c)) == 0:
@@ -39,3 +54,13 @@ class database:
                 return False
         self.conn.commit()
         self.conn.close()
+    
+    def add_new_twitter_user_database(self, database_name):
+        self.conn = sqlite3.connect('accounts.db')
+        self.c = self.conn.cursor()
+        conn = sqlite3.connect('twitter.db')
+        c = conn.cursor()
+        command = "CREATE TABLE IF NOT EXISTS " + database_name + " (tweet_id INTEGER PRIMARY KEY)"
+        c.execute(command)
+        conn.commit()
+        conn.close()
