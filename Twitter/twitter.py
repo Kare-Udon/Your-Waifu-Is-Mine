c9ca185a8cf16ae71c4930ed8dbac47635604700
Lao-Liu233 <laoliu735@gmail.com>
2022-04-03 12:34:08 +0800

    Add: Twitter.py

---
+++ Twitter/twitter.py
@@ -0,0 +1,33 @@
+import tweepy
+import requests
+
+class Twitter:
+    def __init__(self):
+        self.client = tweepy.Client("", return_type='json')
+    
+    def get_new_tweets_of_user(self, twitter_id):
+        tweets = self.client.get_users_tweets(
+                                    id= twitter_id,
+                                    exclude=['retweets','replies'],
+                                    expansions=['attachments.media_keys'],
+                                    media_fields=['url', 'preview_image_url'],
+                                    max_results=10
+                                    )
+        data = tweets.data
+        if tweets.includes:
+            media = tweets.includes['media']
+        else:
+            return ([], None)
+        tweets_with_media = []
+        for tweet in data:
+            if tweet['attachments'] != None:
+                tweets_with_media.append(tweet)   
+        return (tweets_with_media, media)
+    
+    def url_to_id(self, username):
+        header = {
+            "content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+        }
+        data_raw = "input=%40" + username + ""
+        res = requests.post('https://tweeterid.com/ajax.php', data=data_raw, headers=header)
+        return res.text
