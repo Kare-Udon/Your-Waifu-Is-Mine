43b84a093f5ab44abb68333ed8c5615ff928555c
Lao_Liu <laoliu735@gmail.com>
2022-07-06 23:04:29 +0800

    Update: Move settings.json and DB files
    Move settings.json and Database files to data/ folder, for using Docker.

--- sql/sqlite.py
+++ sql/sqlite.py
@@ -7,8 +7,8 @@ class database:
         pass
 
     def init_database(self):
-        if not os.path.exists('./sql/accounts.db'):
-            conn = sqlite3.connect('./sql/accounts.db')
+        if not os.path.exists('./data/accounts.db'):
+            conn = sqlite3.connect('./data/accounts.db')
             c = conn.cursor()
             c.execute(
                 "CREATE TABLE IF NOT EXISTS twitter_users (id TEXT PRIMARY KEY, name TEXT)")
@@ -16,8 +16,8 @@ class database:
                 "CREATE TABLE IF NOT EXISTS pixiv_users (id TEXT PRIMARY KEY, name TEXT)")
             conn.commit
             conn.close()
-        if not os.path.exists('./sql/records.db'):
-            conn = sqlite3.connect('./sql/records.db')
+        if not os.path.exists('./data/records.db'):
+            conn = sqlite3.connect('./data/records.db')
             c = conn.cursor()
             c.execute(
                 "CREATE TABLE IF NOT EXISTS twitter_records (tweet_id TEXT PRIMARY KEY, user_id TEXT)")
@@ -29,7 +29,7 @@ class database:
     # Twitter
 
     def get_all_twitter_user_info(self):
-        conn = sqlite3.connect('./sql/accounts.db')
+        conn = sqlite3.connect('./data/accounts.db')
         c = conn.cursor()
         c.execute("SELECT name,id FROM twitter_users")
         return_data = c.fetchall()
@@ -38,7 +38,7 @@ class database:
         return return_data
 
     def add_twitter_user(self, twitter_id, name):
-        conn = sqlite3.connect('./sql/accounts.db')
+        conn = sqlite3.connect('./data/accounts.db')
         c = conn.cursor()
         c.execute("SELECT id FROM twitter_users WHERE id=?", (twitter_id,))
         if len(list(c)) == 0:
@@ -51,7 +51,7 @@ class database:
             return False
 
     def del_twitter_user(self, id):
-        conn = sqlite3.connect('./sql/accounts.db')
+        conn = sqlite3.connect('./data/accounts.db')
         c = conn.cursor()
         c.execute("SELECT name FROM twitter_users WHERE id=?", (id,))
         db_name = c.fetchall()[0][0]
@@ -64,7 +64,7 @@ class database:
             return False
 
     def add_new_tweet(self, tweet_id, user_id):
-        conn = sqlite3.connect('./sql/records.db')
+        conn = sqlite3.connect('./data/records.db')
         c = conn.cursor()
         command = "SELECT tweet_id FROM twitter_records WHERE tweet_id=" + \
             str(tweet_id)
@@ -80,7 +80,7 @@ class database:
             return False
 
     def shorten_twitter_db(self, user_id):
-        conn = sqlite3.connect('./sql/records.db')
+        conn = sqlite3.connect('./data/records.db')
         c = conn.cursor()
         command = "DELETE FROM twitter_records WHERE tweet_id IN ( SELECT tweet_id FROM ( SELECT tweet_id FROM twitter_records WHERE user_id='" + \
             user_id + "' ORDER BY tweet_id DESC LIMIT 30, 100) a);"
@@ -91,7 +91,7 @@ class database:
     # Pixiv
 
     def get_all_pixiv_user_info(self):
-        conn = sqlite3.connect('./sql/accounts.db')
+        conn = sqlite3.connect('./data/accounts.db')
         c = conn.cursor()
         c.execute("SELECT name,id FROM pixiv_users")
         return_data = c.fetchall()
@@ -100,7 +100,7 @@ class database:
         return return_data
 
     def add_pixiv_user(self, pixiv_id, name):
-        conn = sqlite3.connect('./sql/accounts.db')
+        conn = sqlite3.connect('./data/accounts.db')
         c = conn.cursor()
         c.execute("SELECT id FROM pixiv_users WHERE id=?", (pixiv_id,))
         if len(list(c)) == 0:
@@ -114,7 +114,7 @@ class database:
             return False
 
     def del_pixiv_user(self, id):
-        conn = sqlite3.connect('./sql/accounts.db')
+        conn = sqlite3.connect('./data/accounts.db')
         c = conn.cursor()
         c.execute("SELECT name FROM pixiv_users WHERE id=?", (id,))
         db_name = c.fetchall()[0][0]
@@ -127,7 +127,7 @@ class database:
             return False
 
     def add_new_post(self, post_id, user_id):
-        conn = sqlite3.connect('./sql/records.db')
+        conn = sqlite3.connect('./data/records.db')
         c = conn.cursor()
         c.execute("SELECT post_id FROM pixiv_records WHERE post_id=" + str(post_id))
         if len(list(c)) == 0:
@@ -140,7 +140,7 @@ class database:
             return False
 
     def shorten_pixiv_db(self, user_id):
-        conn = sqlite3.connect('./sql/records.db')
+        conn = sqlite3.connect('./data/records.db')
         c = conn.cursor()
         command = "DELETE FROM pixiv_records WHERE post_id IN ( SELECT post_id FROM ( SELECT post_id FROM pixiv_records WHERE user_id='" + \
             user_id + "' ORDER BY post_id DESC LIMIT 30, 100) a);"
